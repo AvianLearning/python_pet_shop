8e0e24a40c6c2a0627cad1926a6b270d9e01fcc4
AvianLearning <iaradcliffe@hotmail.com>
2020-09-11 15:52:19 +0100

    Added functions to get and update cash

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -3,3 +3,14 @@ def get_pet_shop_name(pet_shop_dict):
     name = pet_shop_dict["name"]
     return name
 
+
+def get_total_cash(pet_shop_dict):
+    sum = pet_shop_dict["admin"]["total_cash"]
+    return sum
+
+
+def add_or_remove_cash(pet_shop_dict, cash_amount):
+    pet_shop_dict["admin"]["total_cash"] = get_total_cash(pet_shop_dict) + cash_amount
+    
+
+
