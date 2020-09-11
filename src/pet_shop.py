42ea4fbf3822d1c5b0e149c25f52736cf92e313e
AvianLearning <iaradcliffe@hotmail.com>
2020-09-11 20:26:40 +0100

    Added functions to check if customer has sufficient funds to buy pet

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -75,6 +75,12 @@ def add_pet_to_customer(customer, new_pet):
     pets = customer['pets']
     pets.append(new_pet)
     
+
+def customer_can_afford_pet(customer, new_pet):
+    if get_customer_cash(customer) >= new_pet['price']:
+        return True
+    else:
+        return False
     
        
 
