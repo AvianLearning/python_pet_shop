e1342b3e8f983765b78e162abf94ab2f61fad311
AvianLearning <iaradcliffe@hotmail.com>
2020-09-11 16:21:27 +0100

    Add function to increase pets sold

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -10,12 +10,18 @@ def get_total_cash(pet_shop_dict):
 
 
 def add_or_remove_cash(pet_shop_dict, cash_amount):
-    pet_shop_dict["admin"]["total_cash"] = get_total_cash(pet_shop_dict) + cash_amount
+    new_total = get_total_cash(pet_shop_dict) + cash_amount
+    pet_shop_dict["admin"]["total_cash"] = new_total
     
 
 def get_pets_sold(pet_shop_dict):
     sold = pet_shop_dict["admin"]["pets_sold"]
     return sold
+
+
+def increase_pets_sold(pet_shop_dict, num_sold):
+    sold = get_pets_sold(pet_shop_dict) + num_sold
+    pet_shop_dict["admin"]["pets_sold"] = sold
 
 
 
